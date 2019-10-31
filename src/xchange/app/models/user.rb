13fd3f1ad092a95fbcc2d9372d8aefb16c26f074
srahooa <sarahoaldrich@gmail.com>
2019-10-31 15:58:24 +1000

    sorted prob w bulma, added rolify

--- src/xchange/app/models/user.rb
+++ src/xchange/app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
+
+  rolify
+  after_create :assign_default_role
+
+  has_many :products
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def assign_default_role
+    add_role(:user) if roles.blank?
+  end
+
 end
