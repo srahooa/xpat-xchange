fca37b75ecbaeafe3cc53395af3fc4d15638c911
srahooa <sarahoaldrich@gmail.com>
2019-11-05 11:28:20 +1000

    added cloudinary, fixed show and edit product pages

--- src/app/controllers/products_controller.rb
+++ src/app/controllers/products_controller.rb
@@ -14,6 +14,8 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     @product.user = current_user
+    @product.image.attach(product_params[:image])
+
     if @product.save
       flash[:alert] = "Your product has been posted for sale."
       redirect_to product_path(@product)
@@ -24,16 +26,25 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id])
   end
 
   def show
-    @product = Product.find(params[:id]) 
+    # @product = Product.find(params[:id]) 
   end
 
   def update
     @product.update(product_params)
+    @product.image.attach(product_params[:image]) if product_params[:image]
     redirect_to product_path
+  end
+
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
@@ -42,7 +53,14 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def authorise
+    return if @product.can_edit?(current_user)
+
+    flash[:alert] = "You are not authorised to perfrom that action!"
+    redirect_to products_path
+  end
+
   def product_params
-    params.require(:product).permit(:product_name, :origin_country, :voltage, :plug_type, :condition, :current_country, :current_city, :price)
+    params.require(:product).permit(:product_name, :origin_country, :voltage, :plug_type, :condition, :current_country, :current_city, :price, :image)
   end
 end
--- src/app/models/product.rb
+++ src/app/models/product.rb
@@ -1,6 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   validates :product_name, presence: true
   validates :price, presence: true, numericality: true 
+
+  def can_edit?(user)
+    return user == self.user || user.has_role?(:admin)
+  end
+
+
 end
